eb4b16d5caa11f3f5a53a666d54faa1627746666
Synthird <105394020+Synthird@users.noreply.github.com>
2025-04-17 16:30:39 -0700

    Format userservice package info code

--- src/userservice/package-info.java
+++ src/userservice/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Classes for manipulating a user. Currently, only the User class is in this package.
+ * Classes for manipulating a user. Currently, only the User class is in this
+ * package.
  * 
  * @author Synthird
  * @version 1.0.0
